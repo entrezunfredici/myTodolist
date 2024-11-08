1b07b4cec0041f635794e4580f4bea5428382228
entrezunfredici <superfred2468@gmail.com>
2024-11-08 09:27:37 +0100

    [WIP] TU

--- todolist_api/tests/users.test.js
+++ todolist_api/tests/users.test.js
@@ -1,22 +1,25 @@
 const request = require('supertest');
 const app = require('../app');  // Importez l'application Express
-const { expect } = require('chai');
 
 // Exemple de test pour la route GET /api/users
-describe('GET /api/users', () => {
+describe('GET http://localhost:9000/users/', () => {
   it('devrait retourner une liste d\'utilisateurs', async () => {
-    const res = await request(app).get('/api/users');
+    const res = await request(app).get('http://localhost:9000/users/');
     expect(res.status).to.equal(200);
     expect(res.body).to.be.an('array'); // Assurez-vous que la réponse est un tableau
   });
 });
 
 // Exemple de test pour la route POST /api/users
-describe('POST /api/users', () => {
+describe('POST http://localhost:9000/users/register', () => {
   it('devrait créer un nouvel utilisateur', async () => {
-    const user = { name: 'Test User', email: 'test@example.com' };
-    const res = await request(app).post('/api/users').send(user);
+    const user = { username:"Jean-kévin",
+      password:"fougères",
+      email:"kévin.fortnite@gmail.com" };
+    const res = await request(app).post('http://localhost:9000/users/register').send(user);
     expect(res.status).to.equal(201);
-    expect(res.body).to.include({ name: 'Test User', email: 'test@example.com' });
+    expect(res.body).to.include({ username:"Jean-kévin",
+    password:"fougères",
+    email:"kévin.fortnite@gmail.com" });
   });
 });
